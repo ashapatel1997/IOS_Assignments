5cf79c941a989a3abe9eb48169e73b48adce9060
Asha <asha@Ashas-Mac.local>
2019-03-22 05:08:57 -0700

    Assignment 4

--- Assignment_4/Assignment_4/ViewController.h
+++ Assignment_4/Assignment_4/ViewController.h
@@ -7,9 +7,39 @@
 //
 
 #import <UIKit/UIKit.h>
-
+int operation;
+double displayNumber;
+double resultValue;
+BOOL isDecimal;
 @interface ViewController : UIViewController
+{
+    
+    IBOutlet UILabel *result;
+}
+-(void) setResultWithNumber:(int)theNumber;
+-(void) operationWithNumber:(int)theNumber;
 
+- (IBAction)clear:(id)sender;
+
+- (IBAction)seven:(id)sender;
+- (IBAction)eight:(id)sender;
+- (IBAction)nine:(id)sender;
+- (IBAction)divide:(id)sender;
+
+- (IBAction)four:(id)sender;
+- (IBAction)five:(id)sender;
+- (IBAction)six:(id)sender;
+- (IBAction)multiply:(id)sender;
+
+- (IBAction)one:(id)sender;
+- (IBAction)two:(id)sender;
+- (IBAction)three:(id)sender;
+- (IBAction)minus:(id)sender;
+
+- (IBAction)zero:(id)sender;
+- (IBAction)dot:(id)sender;
+- (IBAction)equals:(id)sender;
+- (IBAction)plus:(id)sender;
 
 @end
 
--- Assignment_4/Assignment_4/ViewController.m
+++ Assignment_4/Assignment_4/ViewController.m
@@ -16,13 +16,175 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    isDecimal=false;
+    resultValue=0;
+    
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)setResultWithNumber:(int)theNumber{
+    
+    if(!isDecimal){
+        displayNumber *= 10;
+        displayNumber += theNumber;
+        result.text = [NSString stringWithFormat:@"%.0f",displayNumber];
+    }
+    else{
+        result.text = [result.text stringByAppendingString:[NSString stringWithFormat:@"%d", theNumber]];
+    }
+    displayNumber = [result.text floatValue];
+}
+
+-(void)operationWithNumber:(int)theNumber{
+    
+    isDecimal = false;
+    
+    if(resultValue == 0){
+        resultValue = displayNumber;
+    }
+    
+    else{
+        result.text = [NSString stringWithFormat:@"%.2f",resultValue];
+        switch (operation) {
+            case 1:
+                resultValue += displayNumber;
+                break;
+            case 2:
+                resultValue -= displayNumber;
+                break;
+            case 3:
+                resultValue = displayNumber*resultValue;
+                break;
+            case 4:
+                resultValue /= displayNumber;
+                break;
+                
+            default:
+                break;
+        }
+    }
+    operation = theNumber;
+    displayNumber = 0;
+    
+}
+
+
+
+
+- (IBAction)divide:(id)sender
+{
+    if(resultValue != 0){
+        [self operationWithNumber:operation];
+        result.text = [NSString stringWithFormat:@"%.2f",resultValue];
+        displayNumber = [result.text floatValue];
+        resultValue = 0;
+    }
+    [self operationWithNumber:4];
+}
+
+- (IBAction)multiply:(id)sender
+{
+    if(resultValue != 0){
+        [self operationWithNumber:operation];
+        result.text = [NSString stringWithFormat:@"%.2f",resultValue];
+        displayNumber = [result.text floatValue];
+        resultValue = 0;
+    }
+    [self operationWithNumber:3];
+}
+
+- (IBAction)minus:(id)sender
+{
+    if(resultValue != 0){
+        [self operationWithNumber:operation];
+        result.text = [NSString stringWithFormat:@"%.2f",resultValue];
+        displayNumber = [result.text floatValue];
+        resultValue = 0;
+    }
+    [self operationWithNumber:2];
+}
+
+- (IBAction)plus:(id)sender {
+    if(resultValue != 0){
+        [self operationWithNumber:operation];
+        result.text = [NSString stringWithFormat:@"%.2f",resultValue];
+        displayNumber = [result.text floatValue];
+        resultValue = 0;
+    }
+    [self operationWithNumber:1];
+}
+-(IBAction)dot:(id)sender {
+    isDecimal = true;
+    NSRange range = [result.text rangeOfString:@"."];
+    if (range.location ==NSNotFound){
+        result.text = [result.text stringByAppendingString:@"."];
+    }
+}
+
+- (IBAction)equals:(id)sender {
+    
+    [self operationWithNumber:operation];
+    result.text = [NSString stringWithFormat:@"%.2f",resultValue];
+    displayNumber = [result.text floatValue];
+    resultValue = 0;
+}
+
+
+
+- (IBAction)clear:(id)sender {
+    operation = 0;
+    resultValue = 0;
+    displayNumber = 0;
+    isDecimal = false;
+    result.text = [NSString stringWithFormat:@"%i",0];
+}
+
+- (IBAction)seven:(id)sender {
+    [self setResultWithNumber:7];
+}
+
+- (IBAction)eight:(id)sender {
+    [self setResultWithNumber:8];
+}
+
+- (IBAction)nine:(id)sender {
+    [self setResultWithNumber:9];
+}
+
+
+- (IBAction)four:(id)sender {
+    [self setResultWithNumber:4];
+}
+
+- (IBAction)five:(id)sender {
+    [self setResultWithNumber:5];
+}
+
+- (IBAction)six:(id)sender {
+    [self setResultWithNumber:6];
+}
+
+
+
+- (IBAction)one:(id)sender {
+    [self setResultWithNumber:1];
+}
+
+- (IBAction)two:(id)sender {
+    [self setResultWithNumber:2];
+}
+
+- (IBAction)three:(id)sender {
+    [self setResultWithNumber:3];
+}
+
+
+- (IBAction)zero:(id)sender {
+    [self setResultWithNumber:0];
 }
 
 
